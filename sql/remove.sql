8b5320e662656c2b257a6530536069d559d9af94
TonyDoReMi <117762250+TonyDoReMi@users.noreply.github.com>
2023-11-03 16:03:12 -0400

    Completed all tables according to the ERD

---
+++ sql/remove.sql
@@ -0,0 +1,9 @@
+--DROP TABLE AREA 
+DROP TABLE Reviews;
+DROP TABLE Project_Orders;
+DROP TABLE Warehouse_Products;
+DROP TABLE Stores;
+DROP TABLE Warehouse;
+DROP TABLE Products;
+DROP TABLE Project_Customers;
+DROP TABLE Cities;
