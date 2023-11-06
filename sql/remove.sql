890cf586b20ec751b8fa9460130474e5ccdbcec1
TonyDoReMi <117762250+TonyDoReMi@users.noreply.github.com>
2023-11-05 21:25:25 -0500

    Started on Week 2, added checkFlagged, Triggers, AuditLog tables, and UpdateStockQuantity, also added the skeleton for other programs

--- sql/remove.sql
+++ sql/remove.sql
@@ -7,4 +7,18 @@ DROP TABLE Warehouse;
 DROP TABLE Products;
 DROP TABLE Project_Customers;
 DROP TABLE Cities;
+DROP TABLE OrderAuditLog;
+DROP TABLE LoginAuditLog;
+DROP TABLE StockUpdateAuditLog;
+
+
+-- DROP PROCEDURE AREA
+DROP PROCEDURE GetTotalStockForProduct;
+DROP PROCEDURE IsProductAvailable;
+DROP PROCEDURE UpdateStockQuantity;
+DROP PROCEDURE CheckFlaggedReviews;
+
+-- ALTER TRIGGER AREA
+ALTER TRIGGER OrderPlacedTrigger DISABLE;
+ALTER TRIGGER StockUpdateTrigger DISABLE;
 
--- sql/setup.sql
+++ sql/setup.sql
@@ -105,6 +105,41 @@ product_id NUMBER,
     CONSTRAINT fk_product_review 
         FOREIGN KEY (product_id) REFERENCES Products (product_id)
 );
+
+CREATE TABLE OrderAuditLog (
+    OAL_log_id NUMBER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    event_timestamp TIMESTAMP,
+    customer_id NUMBER,
+    product_id NUMBER,
+    event_description VARCHAR2(200),
+    
+    CONSTRAINT pk_OAL
+        PRIMARY KEY (OAL_log_id)
+    
+);
+
+CREATE TABLE LoginAuditLog (
+    LOL_log_id NUMBER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    event_timestamp TIMESTAMP,
+    user_id NUMBER,
+    event_description VARCHAR2(200),
+    
+    CONSTRAINT pk_LOL
+        PRIMARY KEY (LOL_log_id)
+);
+
+CREATE TABLE StockUpdateAuditLog (
+    SUOL_log_id NUMBER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    event_timestamp TIMESTAMP,
+    product_id NUMBER,
+    previous_stock NUMBER,
+    new_stock NUMBER,
+    event_description VARCHAR2(200),
+    
+    CONSTRAINT pk_SUOL
+        PRIMARY KEY (SUOL_log_id)
+);
+
 COMMIT;
 /
 /*INSERTING DATA AREA*/
@@ -199,3 +234,121 @@ INSERT INTO Warehouse(warehouse_name, warehouse_address, city)
     VALUES('warehouse E', '1231 Trudea road', 'ottawa');
 INSERT INTO Warehouse(warehouse_name, warehouse_address)
     VALUES('warehouse F', '16  Whitlock Rd');
+    
+    
+-- Sub-programs AREA
+
+
+-- KEVIN WU
+
+/* Function that checks for the total stocks for a product */
+/* CREATE OR REPLACE FUNCTION GetTotalStockForProduct(
+    some_variable NUMBER
+) 
+RETURN NUMBER AS 
+    
+BEGIN
+    
+    RETURN 1;
+END GetTotalStockForProduct;
+/
+
+/* Function that checks if a product is available or not */
+/*CREATE OR REPLACE FUNCTION IsProductAvailable(
+    some_variable NUMBER
+)
+RETURN NUMBER AS 
+    some_variable NUMBER;
+BEGIN
+    RETURN;
+END IsProductAvailable;
+/ 
+
+
+
+
+
+
+
+
+
+
+-- TONY DO
+
+/* Procedure to update the stock quantity when a customer orders a product */
+CREATE OR REPLACE PROCEDURE UpdateStockQuantity(
+    p_customer_id NUMBER,
+    p_product_id NUMBER,
+    p_order_quantity NUMBER
+
+) AS
+BEGIN
+    UPDATE Warehouse_Products
+    SET total_quantity = total_quantity - p_order_quantity
+    WHERE warehouse_id = (SELECT warehouse_id FROM Project_Customers WHERE customer_id = p_customer_id
+    AND product_id = p_product_id);
+    
+    COMMIT;
+END UpdateStockQuantity;
+/
+
+/* Checks for reviews that are flagged */
+CREATE OR REPLACE PROCEDURE CheckFlaggedReviews AS 
+BEGIN 
+    DECLARE 
+        v_review_id NUMBER;
+        v_description VARCHAR2(200);
+    BEGIN
+    
+    FOR review_rec IN (SELECT review_id, description FROM Reviews WHERE flag = 1) 
+        LOOP
+        v_review_id := review_rec.review_id;
+        v_description := review_rec.description;
+        
+        dbms_output.put_line('Flagged Review ID: ' || v_review_id);
+        dbms_output.put_line('Description: ' || v_description);
+        END LOOP;
+    END;
+END CheckFlaggedReviews;
+/
+
+CREATE OR REPLACE TRIGGER OrderPlacedTrigger
+AFTER INSERT ON Project_Orders
+FOR EACH ROW
+BEGIN
+    INSERT INTO OrderAuditLog (event_timestamp, customer_id, product_id, event_description)
+    VALUES (SYSTIMESTAMP, :new.customer_id, :new.product_id, 'Customer placed an order');
+END;
+/
+
+CREATE OR REPLACE TRIGGER StockUpdateTrigger
+AFTER UPDATE ON Warehouse_Products
+FOR EACH ROW
+BEGIN
+    IF :new.total_quantity <> :old.total_quantity THEN
+        INSERT INTO StockUpdateAuditLog (event_timestamp, product_id, previous_stock, new_stock, event_description)
+        VALUES (SYSTIMESTAMP, :new.product_id, :old.total_quantity, :new.total_quantity, 'Stock updated');
+    END IF;
+END;
+/
+
+
+
+
+-- HARIS HUSSAIN
+
+/* Anonymous block for testing */
+/*DECLARE
+     v_product_id NUMBER := 1; 
+     v_available NUMBER;
+BEGIN 
+    -- checking availability 
+    v_available := IsProductAvailable(v_product_id);
+    IF v_available = 0 THEN
+        dbms_output.put_line('That product isnt available');
+    END IF;
+    
+    -- checking for flags 
+    CheckFlaggedReviews;
+END; */
+/
