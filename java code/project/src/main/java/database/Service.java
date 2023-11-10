ce0d35b7fd61782e2ebd5d0be4e7972753a717c5
wuKevin <wukevin1200@gmail.com>
2023-11-10 12:25:09 -0500

    add connection methods

---
+++ java code/project/src/main/java/database/Service.java
@@ -0,0 +1,60 @@
+package database;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Service {
+
+    private Connection conn;
+    static Scanner scan = new Scanner(System.in);
+
+    public Service() {
+    }
+
+    public Service(String username, String password) throws SQLException {
+
+        String url = "jdbc:oracle:thin:@198.168.52.211: 1521/pdbora19c.dawsoncollege.qc.ca";
+
+        try {
+            this.conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to database");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Connection getConnection() throws SQLException {
+        Connection conn = null;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter username: ");
+        String username = scan.next();
+        System.out.println("Enter password: ");
+        String password = scan.next();
+        scan.close();
+
+        String url = "jdbc:oracle:thin:@198.168.52.211: 1521/pdbora19c.dawsoncollege.qc.ca";
+        conn = DriverManager.getConnection(url, username, password);
+        System.out.println("Connected to database");
+        return conn;
+    }
+
+    public void Close() throws SQLException {
+        if (!this.conn.isClosed()) {
+            this.conn.close();
+            System.out.println("Disconnected to database");
+        }
+    }
+
+    public void AddCourse(Courses myCourses) throws SQLException, ClassNotFoundException {
+
+        myCourses.AddToDatabase(this.conn);
+
+        Close();
+
+    }
+}
+
+
+}
