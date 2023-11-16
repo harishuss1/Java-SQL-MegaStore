a559412ea1ff1c0676f47c08fd9cb5bd12bfa03b
TonyDoReMi <117762250+TonyDoReMi@users.noreply.github.com>
2023-11-15 20:43:17 -0500

    Added some tests for displaying stocks, needs function getTotalProducts first

--- java code/project/src/main/java/database/App.java
+++ java code/project/src/main/java/database/App.java
@@ -1,34 +1,31 @@
 package database;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Display display  = new Display(connection);
-        display.Greet();
         try {
             Connection connection = Service.getConnection();
-            display.DisplayOptions();
-            String user_choice = scan.nextLine();
-    
-            if(user_choice.equals("YES")) {
-                  displayData.displayAllProducts();
 
-                /* call display method */
+            ResultSet resultSet = Stocks.getTotalStockForAllProducts(connection);
+  
+            while (resultSet.next()) {
+                int productId = resultSet.getInt("product_id");
+                int totalStock = resultSet.getInt("total_stock");
+
+                System.out.println("Product ID: " + productId + ", Total Stock: " + totalStock);
             }
 
-            DeleteData deleteData = new DeleteData(connection);
-
-            // deleteData.deleteProduct(1);
-            // deleteData.deleteReviews(1);
-            
-
-            // Step 4: Close the database connection
+           
             connection.close();
-        } catch (SQLException e) {
+        } 
+        catch (SQLException e) {
             e.printStackTrace();
         }
     }
 }
+
+
