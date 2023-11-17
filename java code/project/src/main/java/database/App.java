3c2c5934824206cafbb1fc022b259eb8c11c2901
TonyDoReMi <117762250+TonyDoReMi@users.noreply.github.com>
2023-11-17 12:57:53 -0500

    Removed App.java

--- java code/project/src/main/java/database/App.java
+++ java code/project/src/main/java/database/App.java
@@ -1,20 +1,6 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Scanner;
 public class App {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        try {
-            Display.displayLoginMenu();
-            
-            
-        } 
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+
 }
    
