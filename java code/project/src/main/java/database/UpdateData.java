afc0aa6cf8a7206efaae48fd253f001411324069
TonyDoReMi <117762250+TonyDoReMi@users.noreply.github.com>
2023-11-22 20:08:06 -0500

    fixing bugs

--- java code/project/src/main/java/database/UpdateData.java
+++ java code/project/src/main/java/database/UpdateData.java
@@ -2,33 +2,15 @@ package database;
 
 import java.sql.*;
 
-/**
- * The UpdateData class provides methods for updating product and warehouse information in the database.
- * @class
- */
 public class UpdateData {
 
     public static Object updateData;
     private Connection conn;
 
-    /**
-     * Creates an instance of UpdateData with a specified database connection.
-     * @constructor
-     * @param {Connection} conn - The database connection.
-     */
     public UpdateData(Connection conn) {
         this.conn = conn;
     }
 
-    /**
-     * Updates product information in the database.
-     * @function
-     * @param {number} productId - The ID of the product to update.
-     * @param {string} productName - The new name of the product.
-     * @param {number} productPrice - The new price of the product.
-     * @param {string} productCategory - The new category of the product.
-     * @throws {SQLException}
-     */
     public void updateProduct(int productId, String productName, String productCategory) {
         try (CallableStatement callableStatement = conn.prepareCall("{call update_data.update_product(?, ?, ?)}")) {
             callableStatement.setInt(1, productId);
@@ -42,15 +24,6 @@ public class UpdateData {
         }
     }
 
-    /**
-     * Updates warehouse information in the database.
-     * @function
-     * @param {number} warehouseId - The ID of the warehouse to update.
-     * @param {string} warehouseName - The new name of the warehouse.
-     * @param {number} addressId - The new address ID of the warehouse.
-     * @param {number} cityId - The new city ID of the warehouse.
-     * @throws {SQLException}
-     */
     public void updateWarehouse(int warehouseId, String warehouseName, int addressId, int cityId) {
         try (CallableStatement callableStatement = conn
                 .prepareCall("{call update_data.update_warehouse(?, ?, ?, ?)}")) {
